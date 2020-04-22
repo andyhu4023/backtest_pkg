61ec6248ebc08cc886bdc82b3f6939cc6a77d2d9
Andy <andyhu2014@gmail.com>
2020-04-22 17:49:10 +0800

    Rearrange the structures

--- demo strategies/demo_portfolio_rating.py
+++ demo strategies/demo_portfolio_rating.py
@@ -3,14 +3,15 @@ import backtest_pkg as bt
 from pandas_datareader import DataReader
 import pandas as pd 
 
-# Getting adjusted price from yahoo:
+# Define the universe of securities and period of backtesting:
 universe = ['MMM', 'ABBV', 'FB', 'T', 'GOOGL']
-start = pd.datetime(2018,12,31)
+start = pd.datetime(2018, 12, 31)
 end = pd.datetime(2019, 12, 31)
-
+# Using the adjusted price from yahoo for backtesting:
 price_data = DataReader(universe, 'yahoo', start=start, end=end)['Adj Close']
 
-# Construct portfolio weight: (From strategies like MVO)
+# Construct portfolio from analyst rating: 
+# PS: Ratings here are just random numbers.
 port_weight = pd.DataFrame(columns= universe)
 port_weight.loc[pd.to_datetime('2018-12-31'), :] = pd.Series([3, 5, 2, 3, 4], index= universe)
 port_weight.loc[pd.to_datetime('2019-06-28'), :] = pd.Series([1, 2, 5, 3, 4], index=universe)
@@ -19,7 +20,6 @@ port_weight.loc[pd.to_datetime('2019-06-28'), :] = pd.Series([1, 2, 5, 3, 4], in
 benchmark = pd.DataFrame(1, index= port_weight.index, columns=port_weight.columns)
 
 # Backtest process:
-portfolio = bt.portfolio(port_weight, benchmark=benchmark, end_date=end)
-portfolio.set_price(price_data)
+portfolio = bt.portfolio(weight=port_weight, name='Rating portfolio', benchmark=benchmark, benchmark_name='Equal Weight', end_date=end, price=price_data)
+
 bt_result = portfolio.backtest(plot=True)
-print(bt_result.tail())
