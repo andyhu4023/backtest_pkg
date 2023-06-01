628eac3e2d927a01dbc459390e8a1acb9668dd20
Andy <andyhu2014@gmail.com>
2023-06-01 15:15:22 +0800

    Adding test cases

---
+++ test/test_portfolio.py
@@ -0,0 +1,13 @@
+import os
+import unittest
+import pandas as pd
+from pandas.testing import assert_frame_equal, assert_series_equal
+import backtest_pkg as b
+
+
+class test1(unittest.TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
